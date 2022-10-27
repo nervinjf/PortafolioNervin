69cbcc029da46b9118be874fe3e4b984ce8b6829
nervinjf <nervinjflores@gmail.com>
2022-10-27 14:29:40 -0400

    se actualizo

--- src/page/Form.jsx
+++ src/page/Form.jsx
@@ -3,16 +3,16 @@ import { useForm } from 'react-hook-form';
 
 const Form = () => {
 
-    const { register, handleSubmit, reset } = useForm;
+    // const { register, handleSubmit, reset } = useForm;
 
-    const send = (data) => {
-        reset({
-            "name": "",
-            "phone": "",
-            "email": "",
-            "sms": ""
-        });
-    }
+    // const send = (data) => {
+    //     reset({
+    //         "name": "",
+    //         "phone": "",
+    //         "email": "",
+    //         "sms": ""
+    //     });
+    // }
 
 
     return (
@@ -23,20 +23,20 @@ const Form = () => {
             <form name="contact" netlify >
                 <div className='container-form'>
                     <div className='container-form-item'>
-                        <label htmlFor="name">NOMBRE Y APELLIDO</label>
-                        <input type="text" name="name" {...register("name")}/>
+                        <label htmlFor="name" >NOMBRE Y APELLIDO</label>
+                        <input type="text" name="name" /*{...register("name")}*//>
                     </div>
                     <div className='container-form-item'>
                         <label htmlFor="phone">TELEFONO</label>
-                        <input type="tel" id="phone" name="phone" {...register("phone")}/>
+                        <input type="tel" id="phone" name="phone" /*{...register("phone")}*//>
                     </div>
                     <div className='container-form-item'>
                         <label htmlFor="email">CORREO</label>
-                        <input type="email" name="email"{...register("email")}/>
+                        <input type="email" name="email" /*{...register("email")}*//>
                     </div>
                     <div className='container-form-item'>
                         <label htmlFor="sms">MENSAJE</label>
-                        <textarea type="text" colas="1300" rows="4" name="sms" {...register("sms")} />
+                        <textarea type="text" colas="1300" rows="4" name="sms" /*{...register("sms")}*/ />
                     </div>
                     <button type="submit">ENVIAR</button>
                 </div>

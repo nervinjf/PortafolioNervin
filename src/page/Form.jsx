b442a55cbab47ac6ae7993ec9841fd0be761161b
nervinjf <nervinjflores@gmail.com>
2022-10-27 14:31:50 -0400

    se actualizo

--- src/page/Form.jsx
+++ src/page/Form.jsx
@@ -23,19 +23,19 @@ const Form = () => {
             <form name="contact" netlify >
                 <div className='container-form'>
                     <div className='container-form-item'>
-                        <label htmlFor="name" >NOMBRE Y APELLIDO</label>
+                        <label >NOMBRE Y APELLIDO</label>
                         <input type="text" name="name" /*{...register("name")}*//>
                     </div>
                     <div className='container-form-item'>
-                        <label htmlFor="phone">TELEFONO</label>
+                        <label >TELEFONO</label>
                         <input type="tel" id="phone" name="phone" /*{...register("phone")}*//>
                     </div>
                     <div className='container-form-item'>
-                        <label htmlFor="email">CORREO</label>
+                        <label >CORREO</label>
                         <input type="email" name="email" /*{...register("email")}*//>
                     </div>
                     <div className='container-form-item'>
-                        <label htmlFor="sms">MENSAJE</label>
+                        <label >MENSAJE</label>
                         <textarea type="text" colas="1300" rows="4" name="sms" /*{...register("sms")}*/ />
                     </div>
                     <button type="submit">ENVIAR</button>

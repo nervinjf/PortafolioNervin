925224d45a36764082d4fae46541ee633a8b3a16
nervinjf <nervinjflores@gmail.com>
2022-10-27 14:26:51 -0400

    se actualizo

--- src/page/Form.jsx
+++ src/page/Form.jsx
@@ -6,12 +6,12 @@ const Form = () => {
     const { register, handleSubmit, reset } = useForm;
 
     const send = (data) => {
-        reset=({
+        reset({
             "name": "",
             "phone": "",
             "email": "",
             "sms": ""
-        })
+        });
     }
 
 
@@ -20,7 +20,7 @@ const Form = () => {
             <div className='Po-contacto'>
                 <h2>Ponerse en contacto</h2>
             </div>
-            <form name="contact" netlify onSubmit={handleSubmit(send)}>
+            <form name="contact" netlify >
                 <div className='container-form'>
                     <div className='container-form-item'>
                         <label htmlFor="name">NOMBRE Y APELLIDO</label>
